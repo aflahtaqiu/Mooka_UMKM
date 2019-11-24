83663219692da37335bf1b97c08cf9952b4fecd3
miftahunajat <miftahunajat@gmail.com>
2019-11-24 23:59:04 +0700

    init

---
+++ app/src/main/java/com/example/mooka_umkm/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mooka_umkm
+
+import com.example.mooka_umkm.R
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
