b3c003fbfcae440eaaa8a0297271bf416cd172f8
aflahtaqiu <aflahtaqiu@gmail.com>
2020-02-05 19:50:05 +0700

    change base url

--- app/src/main/java/com/example/mooka_umkm/Config.kt
+++ app/src/main/java/com/example/mooka_umkm/Config.kt
@@ -3,7 +3,6 @@ package com.example.mooka_umkm
 
 object Config {
 //    val API_BASE_URL: String = "https://98381ca9.ngrok.io"
-    val API_BASE_URL: String = "https://mooka-api.herokuapp.com/"
-    val PREFNAME = "mooka" +
-            ""
+    val API_BASE_URL: String = "https://mooka-service.appspot.com/"
+    val PREFNAME = "mooka"
 }
